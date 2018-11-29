53b5af350c1ebd9b85d3cc63d95efed68b7a0935
Kevin Ajili <kevin@atollogy.com>
2018-11-28 16:30:56 -0800

    Initial Commit

---
+++ controllers/api.py
@@ -0,0 +1,144 @@
+# Here go your api methods.
+
+
+@auth.requires_signature()
+def add_post():
+    post_id = db.post.insert(
+        post_title=request.vars.post_title,
+        post_content=request.vars.post_content,
+    )
+    # We return the id of the new post, so we can insert it along all the others.
+    return response.json(dict(post_id=post_id))
+
+
+def get_post_list():
+    results = []
+    if auth.user is None:
+        # Not logged in.
+        rows = db().select(db.post.ALL, orderby=~db.post.post_time)
+        for row in rows:
+            results.append(dict(
+                id=row.id,
+                post_title=row.post_title,
+                post_content=row.post_content,
+                post_author=row.post_author,
+                thumb = None,
+            ))
+    else:
+        # Logged in.
+        rows = db().select(db.post.ALL, db.thumb.ALL, db.user_like.ALL,
+                            left=[
+                                db.thumb.on((db.thumb.post_id == db.post.id) & (db.thumb.user_email == auth.user.email)),
+                                db.user_like.on((db.user_like.post_id == db.post.id) & (db.user_like.user_email == auth.user.email)),
+
+                            ],
+                            orderby=~db.post.post_time)
+        for row in rows:
+            results.append(dict(
+                id=row.post.id,
+                post_title=row.post.post_title,
+                post_content=row.post.post_content,
+                post_author=row.post.post_author,
+                thumb = None if row.thumb.id is None else row.thumb.thumb_state,
+                like=False if row.user_like.id is None else True,
+            ))
+    # For homogeneity, we always return a dictionary.
+    return response.json(dict(post_list=results))
+
+
+
+@auth.requires_signature()
+def edit_post():
+    post_id = int(request.vars.post_id)
+    db.post.update_or_insert(
+        (db.post.id == post_id) & (db.post.post_author == auth.user.email),
+        post_title=request.vars.post_title,
+        post_content=request.vars.post_content,
+    )
+    return "ok" # Might be useful in debugging.
+
+# Replies
+@auth.requires_signature()
+def add_reply():
+    reply_id = db.reply.insert(
+        reply_title=request.vars.reply_title,
+        reply_content=request.vars.reply_content,
+        post_id=request.vars.post_id
+    )
+    # We return the id of the new post, so we can insert it along all the others.
+    return response.json(dict(reply_id=reply_id))
+
+
+def get_reply_list():
+    results = []
+    post_id = request.vars.post_id
+    print("post_id:", post_id)
+    if auth.user is None:
+        # Not logged in.
+        rows = db(db.reply.post_id == post_id).select(db.reply.ALL, orderby=~db.reply.reply_time)
+        # rows = db().select(db.reply.ALL, orderby=~db.reply.reply_time)
+
+        for row in rows:
+            results.append(dict(
+                id=row.id,
+                reply_title=row.reply_title,
+                reply_content=row.reply_content,
+                reply_author=row.reply_author,
+                post_id=row.post_id
+
+            ))
+    else:
+        # Logged in.
+        rows = db(db.reply.post_id == post_id).select(db.reply.ALL,
+                            orderby=~db.reply.reply_time)
+        # rows = db().select(db.reply.ALL, orderby=~db.reply.reply_time)
+        for row in rows:
+            print(row)
+            results.append(dict(
+                id=row.id,
+                reply_title=row.reply_title,
+                reply_content=row.reply_content,
+                reply_author=row.reply_author,
+                post_id=row.post_id
+            ))
+    # For homogeneity, we always return a dictionary.
+    return response.json(dict(reply_list=results))
+
+
+
+@auth.requires_signature()
+def edit_reply():
+    reply_id = int(request.vars.reply_id)
+    db.reply.update_or_insert(
+        (db.reply.id == reply_id) & (db.reply.reply_author == auth.user.email),
+        reply_title=request.vars.reply_title,
+        reply_content=request.vars.reply_content,
+    )
+    return "ok" # Might be useful in debugging.
+
+
+@auth.requires_signature()
+def set_thumb():
+    post_id = int(request.vars.post_id)
+    thumb_state = request.vars.thumb
+    db.thumb.update_or_insert(
+        (db.thumb.post_id == post_id) & (db.thumb.user_email == auth.user.email),
+        post_id=post_id,
+        user_email=auth.user.email,
+        thumb_state=thumb_state
+    )
+    return "ok" # Might be useful in debugging.
+
+@auth.requires_signature()
+def set_like():
+    post_id = int(request.vars.post_id)
+    like_status = request.vars.like.lower().startswith('t');
+    if like_status:
+        db.user_like.update_or_insert(
+            (db.user_like.post_id == post_id) & (db.user_like.user_email == auth.user.email),
+            post_id = post_id,
+            user_email = auth.user.email
+        )
+    else:
+        db((db.user_like.post_id == post_id) & (db.user_like.user_email == auth.user.email)).delete()
+    return "ok" # Might be useful in debugging.
---
+++ static/js/default_index.js
@@ -0,0 +1,312 @@
+// This is the js for the default/index.html view.
+var app = function() {
+
+    var self = {};
+
+    Vue.config.silent = false; // show all warnings
+
+    // Extends an array
+    self.extend = function(a, b) {
+        for (var i = 0; i < b.length; i++) {
+            a.push(b[i]);
+        }
+    };
+
+    // Enumerates an array.
+    var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
+
+    self.add_post = function () {
+        // We disable the button, to prevent double submission.
+        $.web2py.disableElement($("#add-post"));
+        var sent_title = self.vue.form_title; // Makes a copy 
+        var sent_content = self.vue.form_content; //
+        $.post(add_post_url,
+            // Data we are sending.
+            {
+                post_title: self.vue.form_title,
+                post_content: self.vue.form_content
+            },
+            // What do we do when the post succeeds?
+            function (data) {
+                // Re-enable the button.
+                $.web2py.enableElement($("#add-post"));
+                // Clears the form.
+                self.vue.form_title = "";
+                self.vue.form_content = "";
+                // Adds the post to the list of posts. 
+                var new_post = {
+                    id: data.post_id,
+                    post_author: current_user_email,
+                    post_title: sent_title,
+                    post_content: sent_content
+                };
+                self.vue.post_list.unshift(new_post);
+                // We re-enumerate the array.
+                self.process_posts();
+            });
+        // If you put code here, it is run BEFORE the call comes back.
+    };
+
+    self.get_posts = function() {
+        $.getJSON(get_post_list_url,
+            function(data) {
+                // I am assuming here that the server gives me a nice list
+                // of posts, all ready for display.
+                console.log("post_list: ", data.post_list)
+                self.vue.post_list = data.post_list;
+                // Post-processing.
+                self.process_posts();
+                console.log("I got my list");
+            }
+        );
+        console.log("I fired the get");
+    };
+
+    self.process_posts = function() {
+        // This function is used to post-process posts, after the list has been modified
+        // or after we have gotten new posts. 
+        // We add the _idx attribute to the posts. 
+        enumerate(self.vue.post_list);
+        // We initialize the smile status to match the like. 
+        self.vue.post_list.map(function (e) {
+            // I need to use Vue.set here, because I am adding a new watched attribute
+            // to an object.  See https://vuejs.org/v2/guide/list.html#Object-Change-Detection-Caveats
+            // The code below is commented out, as we don't have smiles any more. 
+            // Replace it with the appropriate code for thumbs. 
+            // // Did I like it? 
+            // // If I do e._smile = e.like, then Vue won't see the changes to e._smile . 
+             Vue.set(e, '_smile', e.like);
+             Vue.set(e, '_current_thumb', e.thumb);
+             Vue.set(e, 'editing', false);
+             Vue.set(e, 'show_replies', false);
+             Vue.set(e, 'show_reply_form', false);
+        });
+    };
+
+    self.add_reply = function (post_idx) {
+        // We disable the button, to prevent double submission.
+        $.web2py.disableElement($("#add-reply"));
+        var sent_reply_title = self.vue.reply_form_title; // Makes a copy
+        var sent_reply_content = self.vue.reply_form_content; //
+        var post_id = self.vue.post_list[post_idx].id
+        $.post(add_reply_url,
+            // Data we are sending.
+            {
+                reply_title: self.vue.reply_form_title,
+                reply_content: self.vue.reply_form_content,
+                post_id: post_id
+            },
+            // What do we do when the post succeeds?
+            function (data) {
+                // Re-enable the button.
+                $.web2py.enableElement($("#add-reply"));
+                // Clears the form.
+                self.vue.reply_form_title = "";
+                self.vue.reply_form_content = "";
+                // Adds the post to the list of posts.
+                var new_reply = {
+                    id: data.reply_id,
+                    reply_author: current_user_email,
+                    reply_title: sent_reply_title,
+                    reply_content: sent_reply_content,
+                    post_id: post_id
+                };
+                self.vue.reply_list.unshift(new_reply);
+                // We re-enumerate the array.
+                self.process_replies();
+            });
+        // If you put code here, it is run BEFORE the call comes back.
+    };
+
+    self.get_replies = function(post_idx) {
+        var post_id = self.vue.post_list[post_idx].id
+
+        $.getJSON(get_reply_list_url,
+            {
+                post_id: post_id
+            },
+            function(data) {
+                // I am assuming here that the server gives me a nice list
+                // of replies, all ready for display.
+                console.log("reply_list: ", data.reply_list)
+                self.vue.reply_list = data.reply_list;
+                // Post-processing.
+                self.process_replies();
+                console.log("I got my reply list");
+            }
+        );
+        console.log("I fired the reply get");
+    };
+
+    self.process_replies = function() {
+        // This function is used to post-process posts, after the list has been modified
+        // or after we have gotten new posts.
+        // We add the _idx attribute to the posts.
+        enumerate(self.vue.reply_list);
+        // We initialize the smile status to match the like.
+        self.vue.reply_list.map(function (e) {
+            // I need to use Vue.set here, because I am adding a new watched attribute
+            // to an object.  See https://vuejs.org/v2/guide/list.html#Object-Change-Detection-Caveats
+             Vue.set(e, 'editing_reply', false);
+        });
+    };
+
+    self.show_replies = function (post_idx) {
+        var p = self.vue.post_list[post_idx];
+
+        p.show_replies = !p.show_replies;
+        if(p.show_replies) {
+            self.get_replies(post_idx);
+        }
+    }
+
+    self.show_reply_form = function (post_idx) {
+        var p = self.vue.post_list[post_idx];
+
+        p.show_reply_form = !p.show_reply_form;
+    }
+
+    self.edit_post = function (post_idx) {
+        var p = self.vue.post_list[post_idx];
+        p.editing = true;
+    }
+
+    self.submit_edit_post = function (post_idx) {
+        var p = self.vue.post_list[post_idx];
+        // Starts the spinner.
+        $.post(edit_post_url,
+            {
+                post_id: p.id,
+                post_title: p.post_title,
+                post_content: p.post_content
+            },
+            function (data) {
+//                self.process_posts();
+            }
+            );
+        // Not here, the self.vue.title_save_pending = false;
+    };
+
+    self.end_edit_post = function (post_idx) {
+        var p = self.vue.post_list[post_idx];
+        p.editing = false;
+        // We send the title.
+        self.submit_edit_post(post_idx);
+    };
+
+    self.edit_reply = function (reply_idx) {
+        var r = self.vue.reply_list[reply_idx];
+        r.editing_reply = true;
+    }
+
+    self.submit_edit_reply = function (reply_idx) {
+        var r = self.vue.reply_list[reply_idx];
+        // Starts the spinner.
+        $.post(edit_reply_url,
+            {
+                reply_id: r.id,
+                reply_title: r.reply_title,
+                reply_content: r.reply_content
+            },
+            function (data) {
+//                self.process_posts();
+            }
+            );
+        // Not here, the self.vue.title_save_pending = false;
+    };
+
+    self.end_edit_reply = function (reply_idx) {
+        var r = self.vue.reply_list[reply_idx];
+        r.editing_reply = false;
+        // We send the title.
+        self.submit_edit_reply(reply_idx);
+    };
+
+
+    // Thumb change code.
+    self.thumb_mouseout = function (post_idx) {
+        var p = self.vue.post_list[post_idx];
+        p._current_thumb = p.thumb;
+    };
+
+    self.thumb_mouseover = function (post_idx, new_state) {
+        var p = self.vue.post_list[post_idx];
+        p._current_thumb = new_state;
+    };
+
+    self.thumb_click = function (post_idx, new_state) {
+        var p = self.vue.post_list[post_idx];
+        if(new_state != p.thumb){
+            p.thumb = new_state;
+        }
+        else{ //if the thumb being clicked is what is already stored, set to null
+            p.thumb = null;
+        }
+        $.post(set_thumb_url, {
+            post_id: p.id,
+            thumb: p.thumb
+        }); // Nothing to do upon completion.
+    };
+
+
+    // Complete as needed.
+    self.vue = new Vue({
+        el: "#vue-div",
+        delimiters: ['${', '}'],
+        unsafeDelimiters: ['!{', '}'],
+        data: {
+            form_title: "",
+            form_content: "",
+            post_list: [],
+            reply_form_title: "",
+            reply_form_content: "",
+            reply_list: [],
+            show_form: false,
+        },
+        methods: {
+            add_post: self.add_post,
+
+            thumb_mouseover: self.thumb_mouseover,
+            thumb_mouseout: self.thumb_mouseout,
+            thumb_click: self.thumb_click,
+
+            toggle_form: function(){
+                this.show_form = !this.show_form;
+            },
+
+            edit_post: self.edit_post,
+            end_edit_post: self.end_edit_post,
+
+            add_reply: self.add_reply,
+            get_replies: self.get_replies,
+            show_replies: self.show_replies,
+            show_reply_form: self.show_reply_form,
+
+            edit_reply: self.edit_reply,
+            end_edit_reply: self.end_edit_reply,
+
+        }
+
+    });
+
+    // If we are logged in, shows the form to add posts.
+    if (is_logged_in) {
+        $("#add_post").show();
+
+
+    }
+
+    // Gets the posts.
+    self.get_posts();
+
+    return self;
+};
+
+var APP = null;
+
+// No, this would evaluate it too soon.
+// var APP = app();
+
+// This will make everything accessible from the js console;
+// for instance, self.x above would be accessible as APP.x
+jQuery(function(){APP = app();});
