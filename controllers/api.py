dcb5aadab80328e5e5bd81fe53b795b2c58e6ccf
Jessica Del Rio <J.delrio2015@gmail.com>
2018-12-02 22:07:42 -0800

    Added api.py

--- controllers/api.py
+++ controllers/api.py
@@ -41,6 +41,7 @@ def get_post_list():
                 thumb = None if row.thumb.id is None else row.thumb.thumb_state,
                 like=False if row.user_like.id is None else True,
             ))
+
     # For homogeneity, we always return a dictionary.
     return response.json(dict(post_list=results))
 
@@ -100,6 +101,7 @@ def get_reply_list():
                 reply_author=row.reply_author,
                 post_id=row.post_id
             ))
+        print results
     # For homogeneity, we always return a dictionary.
     return response.json(dict(reply_list=results))
 
@@ -150,38 +152,114 @@ def add_house():
     # We return the id of the new post, so we can insert it along all the others.
     return response.json(dict(house_id=house_id))
 
+# chores
+@auth.requires_signature()
+def add_chore():
+    chore_id = db.chore.insert(
+        chore_content=request.vars.chore_content,
+        house_id=request.vars.house_id,
+        chore_author=auth.user.email
+
+    )
+    # We return the id of the new post, so we can insert it along all the others.
+    return response.json(dict(chore_id=chore_id))
+
 def get_chore_list():
-    results = []
-    post_id = request.vars.house_id
+    thechores = []
+    thehouse_id = request.vars.house_id
 
     if auth.user is None:
         # Not logged in. Should the chore have a reply_time?
-        rows = db(db.chore.post_id == post_id).select(db.chore.ALL, orderby=~db.chore.chore_time)
-        # rows = db().select(db.reply.ALL, orderby=~db.reply.reply_time)
-
-        for row in rows:
-            results.append(dict(
-                id=row.id,
-                chore_content=row.chore_content,
-                post_id=row.post_id
-
-            ))
+        # rows = db(db.chore.post_id == post_id).select(db.chore.ALL, orderby=~db.chore.chore_time)
+        # # rows = db().select(db.reply.ALL, orderby=~db.reply.reply_time)
+        #
+        # for row in rows:
+        #     results.append(dict(
+        #         id=row.id,
+        #         chore_content=row.chore_content,
+        #         post_id=row.post_id
+        #
+        #     ))
+        print "hi"
     else:
         # Logged in.
-        rows = db(db.chore.post_id == post_id).select(db.chore.ALL,
+        chores = db(db.chore.house_id == thehouse_id).select(db.chore.ALL,
                             orderby=~db.chore.chore_time)
-        # rows = db().select(db.reply.ALL, orderby=~db.reply.reply_time)
-        for row in rows:
-            print(row)
-            results.append(dict(
-                id=row.id,
-
-                chore_content=row.chore_content,
-
-                post_id=row.post_id
+        for chore in chores:
+            thechores.append(dict(
+                id=chore.id,
+                house_id=chore.house_id,
+                chore_content=chore.chore_content,
+                chore_author=chore.chore_author
             ))
+
     # For homogeneity, we always return a dictionary.
-    return response.json(dict(chore_list=results))
-    
-    
-    
+    return response.json(dict(chore_list=thechores))
+
+@auth.requires_signature()
+def add_house():
+    house_id = db.house.insert(
+        house_name=request.vars.house_name,
+    )
+    hmember_id = db.hmember.insert(
+        hmember_email=request.vars.hmember_email,
+        house_id=house_id
+    )
+    # We return the id of the new post, so we can insert it along all the others.
+    return response.json(dict(house_id=house_id, hmember_id=hmember_id))
+
+def get_house():
+    themembers = []
+    if auth.user is None:
+        # do nothing
+        print "You're not signed in"
+    else:
+        # Logged in.
+        # rows = db().select(db.house.ALL, db.hmember.ALL, db.chore.ALL,
+        #                    left=[
+        #                        db.hmember.on((db.hmember.house_id == db.house.id) & (
+        #                                    db.hmember.hmember_email == auth.user.email)),
+        #                        db.chore.on((db.chore.house_id == db.house.id))
+        #                    ],
+        #                    orderby=~db.house.id)
+        # for row in rows:
+        #     results.append(dict(
+        #         id=row.house.id,
+        #         house_name=row.house.house_name,
+        #         # Probably need something similar for hmember and chore here
+        #
+        #         # thumb = None if row.thumb.id is None else row.thumb.thumb_state,
+        #         # like=False if row.user_like.id is None else True,
+        #     ))
+        memberinfo = db(db.hmember.hmember_email == auth.user.email).select()
+
+        for member in memberinfo:
+            thehouseid = member.house_id
+            houseinfo = None
+            houses = db(db.house.id == thehouseid).select()
+            for house in houses:
+                houseinfo = house.house_name
+            print houseinfo
+            themembers.append(dict(
+                email_name=member.hmember_email,
+                house_id=member.house_id,
+                house_name=houseinfo,
+            ))
+        # rows = db().select(db.house.ALL, orderby=~db.house.id)
+        # for row in rows:
+        #     results.append(dict(
+        #         id=row.house.id,
+        #         house_name=row.house.house_name,
+        #     ))
+    # For homogeneity, we always return a dictionary.
+    return response.json(dict(house_list=themembers))
+
+@auth.requires_signature()
+def add_hmember():
+    hmember_id = db.hmember.insert(
+        hmember_email=request.vars.hmember_email,
+        house_id=request.vars.house_id,
+
+    )
+    # We return the id of the new post, so we can insert it along all the others.
+    return response.json(dict(hmember_id=hmember_id))
